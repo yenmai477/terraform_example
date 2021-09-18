ea3d053c8005ab4794b4ccafbba376e9b9be42df
Yen Mai <yenmai478@gmail.com>
2021-09-18 17:37:26 +0700

    test build 2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,7 +24,7 @@ function Footer() {
     <FooterSection>
       <div className="Container">
         <span>
-          Coded with 💙 🐱‍💻 by{" "}
+          Coded with 💙 by{" "}
           <a
             href="https://github.com/gurupawar//"
             target="_blank"
