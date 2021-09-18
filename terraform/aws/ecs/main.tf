b712500d615b0dab4d6b8a926ce4e36307ddc36f
Yen Mai <yenmai478@gmail.com>
2021-09-18 13:43:46 +0700

    Update terraform ecs

--- terraform/aws/ecs/main.tf
+++ terraform/aws/ecs/main.tf
@@ -265,6 +265,7 @@ resource "aws_ecs_service" "myapp-service" {
   desired_count   = 1
   iam_role        = aws_iam_role.ecs-service-role.arn
   depends_on      = [aws_iam_policy_attachment.ecs-service-attach1]
+  force_new_deployment = true
 
   load_balancer {
     elb_name       = aws_elb.myapp-elb.name
